0dab5407c1daa0bdf40655d90d00d5a6d271d46c
Hector <hotero001@Hectors-MacBook-Pro.fritz.box>
2022-05-27 12:47:26 +0100

    add test suite

---
+++ test/test_stripe_invoice.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+
+class StripeInvoiceTest < Minitest::Test
+  def test_one
+    assert_equal 1,1
+  end
+
+  def test_two
+    assert_equal 2,2
+  end
+
+  def test_three
+    assert_equal 3,3
+  end
+end
