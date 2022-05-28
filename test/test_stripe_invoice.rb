f7c32b863976f31f5af005922f6bc98ac8c142d1
Hector <hotero001@Hectors-MacBook-Pro.fritz.box>
2022-05-28 20:23:41 +0100

    added tests

--- test/test_stripe_invoice.rb
+++ test/test_stripe_invoice.rb
@@ -1,15 +1,37 @@
 require 'minitest/autorun'
+require 'stripe'
+require 'open-uri'
 
-class StripeInvoiceTest < Minitest::Test
-  def test_one
-    assert_equal 1,1
+Stripe.api_key = ENV['API_KEY']
+
+class StripeInvoiceGemTest < Minitest::Test
+  def test_list_length
+    list = Stripe::Invoice.list({limit: 1})
+    assert_equal list.count, 1
   end
 
-  def test_two
-    assert_equal 2,2
+  #helper method to pass invoices into the hash in different scenarios
+  def download_invoices(x, y)
+    x.auto_paging_each do |invoice|
+      if !invoice.invoice_pdf.nil?
+        y[invoice.id] = invoice.invoice_pdf 
+      end
+    end
   end
-
-  def test_three
-    assert_equal 3,3
+  
+  #this test ensures that the invoice count when you pull all invoices is equal to the invoice
+  #count when you pull invoices after an index and invoices before an index, add them together + 1 for the index
+  #as these two sums should be equal
+  def test_case_one
+    invoice_index = ENV['invoice_id']
+    invoice_ids = Hash.new
+    all_id = Hash.new
+    all = Stripe::Invoice.list({limit: 100})
+    starting_after = Stripe::Invoice.list({limit: 100,starting_after: invoice_index})
+    ending_before = Stripe::Invoice.list({limit: 100, ending_before: invoice_index})
+    download_invoices(all, all_id)
+    download_invoices(starting_after,invoice_ids)
+    download_invoices(ending_before,invoice_ids)
+    assert_equal all_id.count,invoice_ids.count + 1
   end
 end
